1e2c87e6341bbab1f076dd5810095f0cbfba1faa
Jared Galyan <galyanja@gmail.com>
2021-10-10 01:48:29 -0400

    .

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ app = Flask(__name__)
 
 app.secret_key = os.getenv("APP_SECRET_KEY")
 
-crypto_key = os.getenv("CRYPTO_KEY")
-cipher_suite = Fernet(bytes(crypto_key, encoding='utf8'))
+crypto_key = bytes(os.getenv("CRYPTO_KEY"), encoding='utf8')
+print(crypto_key)
+cipher_suite = Fernet(crypto_key)
 
 @app.route('/')
 async def home():
@@ -31,64 +32,62 @@ async def editor():
 
     return render_template("editor/editor.html")
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 async def login():
 
     error = {}
 
     if request.method == 'POST':
-        email = request.form['']
-        password = request.form['']
+        email = request.form['email']
+        password = request.form['password']
         async with asqlite.connect("main.db") as conn:
             async with conn.cursor() as cursor:
                 await cursor.execute(f"SELECT * FROM accounts WHERE email = ?", email)
                 result = await cursor.fetchone()
         if result is None:
-            return render_template()
-        elif bytes(password, encoding='utf8') != cipher_suite.decrypt(result[2]):
-            return render_template()
+            error['email'] = 'That email is not registered in our system.'
+            return render_template("login.html", error=error)
+        elif bytes(password, encoding='utf8') != cipher_suite.decrypt(bytes(result[2], encoding='utf8')):
+            error['password'] = 'Password is incorrect.'
+            return render_template("login.html", error=error)
         else:
             session["user"] = str(result[0])
             session["email"] = str(result[2])
             session["account_id"] = str(result[3])
-            return redirect(url_for())
+            return redirect(url_for("home"))
     else:
         if "user" in session:
-            return redirect(url_for())
-        return render_template('login.html', error=error)
+            return redirect(url_for("home"))
+        return render_template("login.html")
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 async def signup():
 
     error = {}
 
     if request.method == 'POST':
-        email = request.form['']
-        password = request.form['']
-        confirm_password = request.form['']
-        username = request.form['']
-        
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirmPassword']
+        username = request.form['username']
         async with asqlite.connect("main.db") as conn:
             async with conn.cursor() as cursor:
                 await cursor.execute(f"SELECT * FROM accounts WHERE email = ?", email)
                 result = await cursor.fetchone()
-
                 if result is not None:
-                    return render_template()
-
+                    error['email'] = 'That email is already registered in our system.'
+                    return render_template("signup.html", error=error)
                 elif password == confirm_password:
                     ciphered_text = cipher_suite.encrypt(bytes(password, encoding='utf8'))
-
                     sql = ("INSERT INTO accounts(email, password, username) VALUES(?,?,?)")
                     val = (str(email), ciphered_text, str(username))
-
                     await cursor.execute(sql, val)
-                    return render_template()
-
+                    return redirect(url_for("home"))
                 else:
-                    return render_template()
+                    error["confirmPassword"] = 'Passwords must match.'
+                    return render_template("signup.html", error=error)
     else:
-        return render_template('signup.html', error=error)
+        return render_template("signup.html")
 
 
 
